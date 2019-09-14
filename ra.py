6c45443d00b3c3c1733c8bfd706adc79f4ba9bef
Guilherme R Pereira <guilherme.rp2014@gmail.com>
2019-09-14 20:08:44 -0300

    added docs

--- ra.py
+++ ra.py
@@ -5,14 +5,29 @@ import numpy as np
 # Constants
 MIN_MATCHES = 15
 
-
 def drawKeypoints(figure, figure_kps):
+    """
+    Draw keypoints over a figure.
+
+    Parameters:
+    - @figure: What image you want to plot the keypoints.
+    - @figure_kps: Keypoints of this image.
+    """
     # draw only keypoints location, not size and orientation
     figure_with_kps = cv2.drawKeypoints(figure, figure_kps, figure_with_kps, color=(0,255,0), flags=0)
     cv2.imshow('Keypoints', figure_with_kps)
     cv2.waitKey(0)
 
 def findHomography(src_keypoints, dst_keypoints, matches):
+    """
+    Calculates and returns the Homography matrix between two planes: the train image plane and the query image plane.
+    Homography is a 3x3 matrix that correlates the points that exist in two planes.
+
+    Parameters:
+    - @src_keypoints: Keypoints from the train image.
+    - @dst_keypoints: Keypoints from the destiny image.
+    - @matches: Matches between the two sets (train and query images).
+    """
 
     # source points
     src_pts = np.float32([
@@ -30,6 +45,18 @@ def findHomography(src_keypoints, dst_keypoints, matches):
     return M
 
 def drawRectangle(source, src_kps, dst_kps, matches, destiny):
+    """
+    Uses the shape of the train image, along with the Homography previously calculated, to 'send' the points
+    from one plane (train image plane) to another (query image plane).
+
+    Parameters:
+    - @source: it is the train image, to retrieve its shape (height and width).
+    - @src_kps: Keypoints from the train image.
+    - @dst_kps: Keypoints from the destiny image.
+    - @matches: Matches found between the train and query images.
+    - @destiny: it is the query image, in which the rectangle is going to be drawn.
+    """
+
     h, w = source.shape # height and width of the image that we are searching
     
     pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2) # reshape(rows, cols, dimension)
