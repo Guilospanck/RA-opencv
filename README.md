0cd012919704283a5e92779011045a56d1bc13e5
Guilherme Pereira <guilospanck@protonmail.com>
2024-12-25 17:36:05 +0000

    Improve code and docs

--- README.md
+++ README.md
@@ -1,22 +1,39 @@
 # RA-opencv
- Augmented Reality with OpenCV using Python
+
+Augmented Reality with OpenCV using Python.
  
- ## Usage
+## Usage
 
-* Place the image of the surface to be tracked inside the `references` folder.
-* On line 221 of `src/ra.py` replace `'marley_source.jpg'` with the name of the image you just copied inside the `reference` folder.
-* On line 232 of `src/ra.py` replace `'fox.obj'` with the name of the model you want to render. To change the size of the rendered model change the scale parameter (number `3`) in line 189 of `src/ra.py` by a suitable number. This might require some trial and error.
-* Open a terminal session inside the project folder and run:
-* `pip install opencv-python`
-* `pip install opencv-contrib-python`
-* `python3 src/ra.py`
+* Place the image of the surface to be tracked inside the `references` folder;
+* Replace `TRAIN_IMAGE_RELATIVE_PATH` with the relative path of the train image you want to track;
+* Replace `OBJ_MODEL_RELATIVE_PATH` with the relative path of the model you want to render. To change the size of the rendered model change the scale parameter `MODEL_OBJ_SCALE` to a suitable number. This might require some trial and error.
 
+## Running 
+
+Open a terminal session inside the project folder and run:
+
+```sh
+# Activate virtualenv
+virtualenv python_ra
+source python_ra/bin/activate
+# to deactivate: run `deactivate`
+```
+Then:
+
+```sh
+# Install from the requirements
+pip install -r requirements.txt
+python3 src/ra.py
+```
 
 ### Command line arguments
 
-* `--rectangle`, `-r`: Draws the projection of the reference surface on the video frame as a blue rectangle.
+* `--rectangle`, `-r`: Draws the projection of the reference surface on the video frame as a blue rectangle;
 * `--matches`, `-ma`: Draws matches between reference surface and video frame.
 
+```sh
+python3 src/ra.py -r -ma
+```
 
 ## Troubleshooting
 
@@ -25,6 +42,7 @@
 ```
 Unable to capture video
 ```
+
 printed to your terminal, the most likely cause is that your OpenCV installation has been compiled without FFMPEG support. Pre-built OpenCV packages such as the ones downloaded via pip are not compiled with FFMPEG support, which means that you have to build it manually.
 
 
--- src/ra.py
+++ src/ra.py
@@ -1,37 +1,41 @@
+#!/usr/bin/python3
 import cv2
 import sys
 import numpy as np
 import math
 import os
-from objloader_simple import *
+from objloader_simple import OBJ
+import platform
 
 import argparse
 
 # Minimum number of matches that have to be found
 # to consider the recognition valid
 MIN_MATCHES = 60
+# Only check for matches every some frames
+FRAME_SKIP = 5
+# Change this to the train image you want to find any match in the video stream
+TRAIN_IMAGE_RELATIVE_PATH = "references/software.jpg"
+# Change this to the .obj model you want to render on the matched image
+OBJ_MODEL_RELATIVE_PATH = "models/fox/fox.obj"
+# Change this to increase/decrease the scale of the rendered .obj
+MODEL_OBJ_SCALE = 3
 
 # Command line argument parsing
-parser = argparse.ArgumentParser(description='Augmented Reality')
+parser = argparse.ArgumentParser(description="Augmented Reality")
 
-parser.add_argument('-r', '--rectangle', help= 'Draw rectangle delimiting target surface on frame.', action='store_true')
-parser.add_argument('-mk', '--model_keypoints', help= 'Draw model keypoints.', action='store_true')
-parser.add_argument('-fk', '--frame_keypoints', help= 'Draw frame keypoints.', action='store_true')
-parser.add_argument('-ma', '--matches', help= 'Draw matches between keypoints.', action='store_true')
+parser.add_argument(
+    "-r",
+    "--rectangle",
+    help="Draw rectangle delimiting target surface on frame.",
+    action="store_true",
+)
+parser.add_argument(
+    "-ma", "--matches", help="Draw matches between keypoints.", action="store_true"
+)
 
 args = parser.parse_args()
 
-
-def drawKeypoints(figure, figure_kps):
-    """
-    Draw keypoints over a figure.
-
-    Parameters:
-    - @figure: What image you want to plot the keypoints.
-    - @figure_kps: Keypoints of this image.
-    """
-    # draw only keypoints location, not size and orientation
-    figure = cv2.drawKeypoints(figure, figure_kps, figure, color=(0,255,0), flags=0)
 
 def findHomography(src_keypoints, dst_keypoints, matches):
     """
@@ -45,19 +49,26 @@ def findHomography(src_keypoints, dst_keypoints, matches):
     """
 
     # source points
-    src_pts = np.float32([
-        src_keypoints[m.queryIdx].pt for m in matches # points on source image in relation to the destination image (query image)
-    ]).reshape(-1, 1, 2)
+    src_pts = np.float32(
+        [
+            src_keypoints[m.queryIdx].pt
+            for m in matches  # points on source image in relation to the destination image (query image)
+        ]
+    ).reshape(-1, 1, 2)
 
     # destination points
-    dst_points = np.float32([
-        dst_keypoints[m.trainIdx].pt for m in matches # points on the destination image in relation to the source image (train image)
-    ]).reshape(-1, 1, 2)
+    dst_points = np.float32(
+        [
+            dst_keypoints[m.trainIdx].pt
+            for m in matches  # points on the destination image in relation to the source image (train image)
+        ]
+    ).reshape(-1, 1, 2)
 
     # compute Homography
-    M, mask = cv2.findHomography(src_pts, dst_points, cv2.RANSAC, 5.0)
+    M, _ = cv2.findHomography(src_pts, dst_points, cv2.RANSAC, 5.0)
 
     return M
+
 
 def drawRectangle(source, src_kps, dst_kps, matches, destiny):
     """
@@ -72,9 +83,11 @@ def drawRectangle(source, src_kps, dst_kps, matches, destiny):
     - @destiny: it is the query image, in which the rectangle is going to be drawn.
     """
 
-    h, w = source.shape # height and width of the image that we are searching
-    
-    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2) # reshape(rows, cols, dimension)
+    h, w = source.shape  # height and width of the image that we are searching
+
+    pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(
+        -1, 1, 2
+    )  # reshape(rows, cols, dimension)
 
     # get Homography
     M = findHomography(src_kps, dst_kps, matches)
@@ -85,10 +98,12 @@ def drawRectangle(source, src_kps, dst_kps, matches, destiny):
     # connect them with lines
     destiny = cv2.polylines(destiny, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
 
+
 def intrinsic_camera(fu, fv, u0, v0):
     K = np.float32([fu, 0, u0, 0, fv, v0, 0, 0, 1])
     K = K.reshape(3, 3)
     return K
+
 
 def projection_matrix(K, H):
     """
@@ -124,7 +139,7 @@ def projection_matrix(K, H):
                 c = R1 + R2    and d = c x p = (R1 + R2) x (R1 x R2)
 
         Now, if this new basis (c, d) is transformed into unit vectors, that is to say c' = c/||c|| and d' = d/||d||,
-        and rotated 45° counterclockwise, we will have a basis that is orthonormal and is pretty close to the 
+        and rotated 45° counterclockwise, we will have a basis that is orthonormal and is pretty close to the
         real values of R1 and R2. So:
 
                 R1' = 1/sqrt(2) * (c/||c||  +  d/||d||)    rotated 45° counterclockwise with values normalized
@@ -133,7 +148,7 @@ def projection_matrix(K, H):
     - Now that we have our orthonormal basis that is close to the real R1 and R2, we can calculate the value of R3:
 
                     R3 = R1' x R2'
-    
+
     - 3D projection matrix:      K * [R1' R2' R3 t]
 
     - For detailed explanation, open the image 'understanding/projection_matrix.png'.
@@ -141,18 +156,22 @@ def projection_matrix(K, H):
     """
 
     # computes G = [G1 G2 G3] = K^-1 * H
-    H = H * (-1) # If don't change this, the obj will render on the wrong axis. E.g.: if a change the book with a rotation forward, the correct was the obj go back, but, with normal H, the object actually shows the wrong face
+    H = H * (
+        -1
+    )  # If don't change this, the obj will render on the wrong axis. E.g.: if a change the book with a rotation forward, the correct was the obj go back, but, with normal H, the object actually shows the wrong face
     G = np.dot(np.linalg.inv(K), H)
 
-    col1 = G[:,0]
-    col2 = G[:,1]
-    col3 = G[:,2]
+    col1 = G[:, 0]
+    col2 = G[:, 1]
+    col3 = G[:, 2]
 
-    # normalize vectors
-    l = math.sqrt(np.linalg.norm(col1, 2) * np.linalg.norm(col2, 2)) # l = sqrt( ||G1|| * ||G2|| )
-    R1 = col1/l
-    R2 = col2/l
-    t = col3/l
+    # normalize vectors (uses geometric mean so no vector has more "weight" than the other)
+    l = math.sqrt(
+        np.linalg.norm(col1, 2) * np.linalg.norm(col2, 2)
+    )  # l = sqrt( ||G1|| * ||G2|| )
+    R1 = col1 / l
+    R2 = col2 / l
+    t = col3 / l
 
     # compute the orthonormal basis 45° clockwise
     c = R1 + R2
@@ -160,8 +179,12 @@ def projection_matrix(K, H):
     d = np.cross(c, p)
 
     # compute the orthonormal basis 45° counterclockwise
-    R1_line = np.dot(1 / math.sqrt(2), c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2)) # R1' = 1/sqrt(2) * ( c/||c|| + d/||d|| )
-    R2_line = np.dot(1 / math.sqrt(2), c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2)) # R2' = 1/sqrt(2) * ( c/||c|| - d/||d|| )
+    R1_line = np.dot(
+        1 / math.sqrt(2), c / np.linalg.norm(c, 2) + d / np.linalg.norm(d, 2)
+    )  # R1' = 1/sqrt(2) * ( c/||c|| + d/||d|| )
+    R2_line = np.dot(
+        1 / math.sqrt(2), c / np.linalg.norm(c, 2) - d / np.linalg.norm(d, 2)
+    )  # R2' = 1/sqrt(2) * ( c/||c|| - d/||d|| )
 
     # compute R3
     R3 = np.cross(R1_line, R2_line)
@@ -172,13 +195,17 @@ def projection_matrix(K, H):
 
     return projection
 
+
 def hex_to_rgb(hex_color):
     """
     Helper function to convert hex strings to RGB
     """
-    hex_color = hex_color.lstrip('#')
+    hex_color = hex_color.lstrip("#")
     h_len = len(hex_color)
-    return tuple(int(hex_color[i:i + h_len // 3], 16) for i in range(0, h_len, h_len // 3))
+    return tuple(
+        int(hex_color[i : i + h_len // 3], 16) for i in range(0, h_len, h_len // 3)
+    )
+
 
 def render(img, obj, projection, model, color=False):
     """
@@ -186,7 +213,7 @@ def render(img, obj, projection, model, color=False):
     """
 
     vertices = obj.vertices
-    scale_matrix = np.eye(3) * 3 # allow us to scale the model
+    scale_matrix = np.eye(3) * MODEL_OBJ_SCALE  # allow us to scale the model
     h, w = model.shape
 
     for face in obj.faces:
@@ -203,33 +230,45 @@ def render(img, obj, projection, model, color=False):
             cv2.fillConvexPoly(img, imgpts, (137, 27, 211))
         else:
             color = hex_to_rgb(face[-1])
-            color = color[::-1] # reverse
+            color = color[::-1]  # reverse
             cv2.fillConvexPoly(img, imgpts, color)
-    
+
     return img
 
-def run():
 
+def getVideoCaptureSettingBasedOnPlatform():
+    system = platform.system()
+    if system == "Windows":
+        return cv2.CAP_DSHOW
+    elif system == "Darwin":
+        return cv2.CAP_AVFOUNDATION
+    elif system == "Linux":
+        return cv2.CAP_V4L2
+    else:
+        print(f"Unknown operating system: {system}")
+        return None
+
+
+def run():
     # gets the current directory
     dir_name = os.getcwd()
-    dir_name = dir_name.split('\\')
+    dir_name = dir_name.split("\\")
     dir_name.pop()
-    dir_name = '\\'.join(dir_name)
-    print(dir_name)
+    dir_name = "\\".join(dir_name)
 
     # gets the train image
-    source = cv2.imread(os.path.join(dir_name,'references/marley_source.jpg'), 0)
+    source = cv2.imread(os.path.join(dir_name, TRAIN_IMAGE_RELATIVE_PATH), 0)
 
     # init video capture
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap = cv2.VideoCapture(0, getVideoCaptureSettingBasedOnPlatform())
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
 
     # Gets the Intrinsic matrix
-    K = intrinsic_camera(fu=800, fv=800, u0=320, v0=240)
+    K = intrinsic_camera(fu=950, fv=950, u0=640, v0=360)
 
     # loads the 3d .obj model using the objoader_simple.py helper
-    obj = OBJ(os.path.join(dir_name, 'models/fox/fox.obj'), swapyz=True)
+    obj = OBJ(os.path.join(dir_name, OBJ_MODEL_RELATIVE_PATH), swapyz=True)
 
     # Initiate orb detector
     orb = cv2.ORB_create()
@@ -238,18 +277,27 @@ def run():
     bf = cv2.BFMatcher_create(cv2.NORM_HAMMING, crossCheck=True)
 
     # Find the keypoints and descriptors of the train image with orb
-    source_kps, source_des = orb.detectAndCompute(source, None) #  keypoints = 500 ; descriptors = 500 with 32 integer values each
+    source_kps, source_des = orb.detectAndCompute(
+        source, None
+    )  #  keypoints = 500 ; descriptors = 500 with 32 integer values each
+
+    frame_count = 0
 
     while True:
         # read the current frame
         ok, frame = cap.read()
         if not ok:
-            print('Cannot read video file')
+            print("Cannot read video file")
             sys.exit()
-            return
+
+        frame_count += 1
+        if frame_count % FRAME_SKIP != 0:
+            continue
 
         # find the keypoints and descriptors of the frame
         frame_kps, frame_des = orb.detectAndCompute(frame, None)
+        if frame_des is None:
+            continue
 
         # match frame descriptors with source descriptors
         matches = bf.match(source_des, frame_des)
@@ -258,10 +306,8 @@ def run():
         # the lower the distance, the better the match
         matches = sorted(matches, key=lambda x: x.distance)
 
-        cv2.imshow('Frame', frame)
-
         # close
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
         # verify if enough matches are found. If yes, compute Homography
@@ -271,35 +317,38 @@ def run():
             # draw rectangle if there is this arg
             if args.rectangle:
                 drawRectangle(source, source_kps, frame_kps, matches, frame)
-            
+
             # if a valid homography was found, render the cube on model plane
             if H is not None:
                 try:
                     # obtain the projection matrix
-                    P = projection_matrix(K, H) 
+                    P = projection_matrix(K, H)
                     # project cube or model
-                    frame = render(frame, obj, P, source, False)                    
-                except:
+                    frame = render(frame, obj, P, source, False)
+                except Exception:
                     pass
-            
+
             if args.matches:
                 # draw the first MIN_MATCHES
-                frame = cv2.drawMatches(source, source_kps, frame, frame_kps, matches[:MIN_MATCHES], 0, flags=2)
-            
-            # show the results
-            cv2.imshow('Frame', frame)
+                frame = cv2.drawMatches(
+                    source,
+                    source_kps,
+                    frame,
+                    frame_kps,
+                    matches[:MIN_MATCHES],
+                    None,
+                    flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS,
+                )
 
-            # close
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        
+            # show the results
+            cv2.imshow("Frame", frame)
+
         else:
             print("Not enough matches found - %d/%d " % (len(matches), MIN_MATCHES))
-    
+
     cap.release()
     cv2.destroyAllWindows()
-    return 0
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
